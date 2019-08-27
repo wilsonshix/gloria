f7b33a88cdf24008681c7ec88b3d7ced015bfec1
Wilson shix <willdjako05@gmail.com>
2019-08-28 01:38:08 +0200

    Modification du pom.xml : Ajout dépendances junit et java-client Appium + Création test Appium "FirstAppiumTest"

---
+++ src/test/java/com/ivorytech/basics/FirstAppiumTest.java
@@ -0,0 +1,33 @@
+package com.ivorytech.basics;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+ 
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class FirstAppiumTest {
+
+public static void main(String[] args) {
+		
+		//Set the Desired Capabilities
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("deviceName", "My Phone");
+		caps.setCapability("udid", "8f81d4e2"); //Give Device ID of your mobile phone
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("platformVersion", "9");  // The version of Android on your device
+		caps.setCapability("appPackage", "com.android.vending");
+		caps.setCapability("appActivity", "com.google.android.finsky.activities.MainActivity");
+		caps.setCapability("noReset", "true");
+		
+		//Instantiate Appium Driver
+		try {
+				AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+			
+		} catch (MalformedURLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
